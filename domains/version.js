50acd9cf024b875976dc5a89af32a5836e7f0bb6
Anusidh Pavamontri <apavamontri@gmail.com>
2017-05-13 13:32:40 +0700

    Add comment to version domain

--- domains/version.js
+++ domains/version.js
@@ -1,5 +1,15 @@
+/**
+ * Version domain: Responsible for displaying current number.
+ * We are following the symantic version number (http://semver.org/)
+ *
+ *   MAJOR version when you make incompatible API changes,
+ *   MINOR version when you add functionality in a backwards-compatible manner, and
+ *   PATCH version when you make backwards-compatible bug fixes.
+ */
 class Version {
   static toString() {
+
+    // Hard-code version number to 1 for now
     return '1.0.0';
   }
 }
